00515d60c4e7914e8d5e60520123568e017bdfa8
TiagoPitanga <tiago.moura.pereira@escola.pr.gov.br>
2026-05-29 15:00:41 +0000

    index

---
+++ script.js
@@ -0,0 +1,8 @@
+// Selecionando o botão do HTML
+const botaoJornada = document.getElementById('btn-jornada');
+
+// Ouvindo o clique do usuário
+botaoJornada.addEventListener('click', function() {
+    console.log('O botão de início foi clicado com sucesso!');
+    alert('Preparado para o grande salto na agrícola? Vamos começar!');
+});
